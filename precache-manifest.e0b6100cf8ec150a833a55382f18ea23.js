9053a0a8691f69d822c5bfb226d4ef59d636490b
Miles Watkins <miles.w.watkins@gmail.com>
2019-04-15 16:11:39 -0400

    Updates

---
+++ precache-manifest.e0b6100cf8ec150a833a55382f18ea23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "322995d29d11e048ef83",
+    "url": "/lunch-bunch/static/js/runtime~main.277a7438.js"
+  },
+  {
+    "revision": "11411795ff9d21f23bee",
+    "url": "/lunch-bunch/static/js/main.e49ed09c.chunk.js"
+  },
+  {
+    "revision": "f5dfc404a3457b78eb99",
+    "url": "/lunch-bunch/static/js/2.c9ee1717.chunk.js"
+  },
+  {
+    "revision": "11411795ff9d21f23bee",
+    "url": "/lunch-bunch/static/css/main.6a2dc99b.chunk.css"
+  },
+  {
+    "revision": "b1069b1bfa6c24ec89bdfcc8a70ce724",
+    "url": "/lunch-bunch/index.html"
+  }
+];
